5e59918535b7af4fec58f743f77675205847b159
Satyam <satus2311@gmail.com>
2022-05-21 20:43:11 +0530

    Added navbar functionality

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,26 +9,30 @@ function Navbar() {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-        <div className={styles.callButton}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-            width="32"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-            />
-          </svg>
+        <div className={styles.homeButton}>
+          <Link href="/" passHref>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              width={30}
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              />
+            </svg>
+          </Link>
         </div>
         <div className={styles.texts}>
           <div className={styles.text}>Query? Call Now !</div>
-          <div className={styles.text}>+918989834549</div>
+          <div className={styles.text}>
+            <a href="tel:+9189898xxx">+9189898xxx</a>
+          </div>
         </div>
       </div>
       <div className={styles.item}>
